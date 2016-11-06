8f7ef193a6e624538412797d76472e206f814c4c
Bert Van Vreckem <bert.vanvreckem@gmail.com>
2016-11-06 12:14:09 +0100

    Verbeteringen provisioning-script: logging & commentaar, testpagina, SELinux-service

--- testopstelling/provision/srvcaddy.sh
+++ testopstelling/provision/srvcaddy.sh
@@ -25,6 +25,9 @@ readonly white='\e[0;37m'
 
 ip=192.168.56.52
 download_dir=/tmp/caddy
+provisioning_files=/vagrant/provision/files
+systemd_service_file=/lib/systemd/system/caddy.service
+
 #}}}
 
 #{{{ Helper functions
@@ -42,40 +45,68 @@ error() {
 info() {
   printf "${yellow}### %s${reset}\n" "${*}"
 }
+
+# Usage: files_differ FILE1 FILE2
+#
+# Tests whether the two specified files have different content
+#
+# Returns with exit status 0 if the files are identical, a nonzero exit status
+# if they differ
+files_differ() {
+  local -r checksum1=$(md5sum "${1}" | cut -c 1-32)
+  local -r checksum2=$(md5sum "${2}" | cut -c 1-32)
+
+  [ "${checksum1}" != "${checksum2}" ]
+}
+
 #}}}
 
 # Script proper
 
-info "Installing Caddy"
+info 'Installing Caddy'
 
 if [ ! -x /usr/local/bin/caddy ]; then
+  # Synchronize package database
   apt-get update
+
+  # Download Caddy installation files 
   wget -O /tmp/caddy.tar.gz 'https://caddyserver.com/download/build?os=linux&arch=amd64&features='
+
+  # Unzip installation archive in temporary directory
   mkdir "${download_dir}"
   pushd "${download_dir}"
   tar xf /tmp/caddy.tar.gz
   popd
   cp "${download_dir}/caddy" /usr/local/bin
 
-  info "Installing Systemd service file"
-
-  cp "${download_dir}/init/linux-systemd/caddy.service" /lib/systemd/system
-  systemctl daemon-reload
+  # Cleanup installation files
+  rm -rf /tmp/caddy*
 else
-  info "  -> Caddy already installed, skipping"
+  info '  -> Caddy already installed, skipping'
+fi
+
+if files_differ "${systemd_service_file}" "${provisioning_files}/caddy.service"; then
+  info 'Copying Systemd service file'
+  cp "${provisioning_files}/caddy.service" "${systemd_service_file}"
+  systemctl daemon-reload
 fi
 
 if [ ! -d /var/www ]; then
-  info " Creating document root"
+  info 'Creating document root'
   mkdir /var/www
   chown -R root:www-data /var/www
   chmod 755 /var/www
 fi
 
+if [ ! -f /var/www/index.html ]; then
+  info 'Copying test homepage'
+  cp "${provisioning_files}/index.html" /var/www
+fi
+
 info 'Configuration'
 
 if [ ! -d /etc/caddy ]; then
-  info "  -> Creating configuration directory"
+  info '  -> Creating configuration directory'
   mkdir /etc/caddy
 fi
 
@@ -88,6 +119,6 @@ https://${ip} {
 }
 _EOF_
 
-info "Starting service"
+info 'Starting service'
 systemctl start caddy
 systemctl enable caddy
